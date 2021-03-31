659716d15dab4fe54eb0f809bf7a06608185e57b
karoline bustinza <karoline.bustinza@tecsup.edu.pe>
2021-03-31 11:21:28 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.get('/',c_inicio);
+app.get('/clientes',c_clientes);
+app.get('/productos',c_productos);
+
+function c_inicio(req,res){
+    res.send('<h1>Express Inic</h1>')
+}
+
+function c_clientes(req,res){
+    res.send('<h1>clientes</h1><br><h3>eduardo Cabroski</h3><br><h3>Rodrigo Gomez</h3><br><h3>nicolas Garcia</h3>')
+}
+function c_productos(req,res){
+    res.send('<h1>productos</h1><br><h3>Gaseosa   1.50</h3><br><h3>Galleta   0.50</h3><br><h3>Chizito   0.50</h3>')
+}
+
+function c_server(req,res){
+    console.log('port:8000');
+}
+
+var server=app.listen(8000,c_server);
